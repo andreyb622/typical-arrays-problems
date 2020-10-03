71165822a32849598969955e3e4253dfbf5af941
andreyb622 <andreyb622@gmail.com>
2020-10-03 20:57:47 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0
+  } else {
+    return array.reduce((sum,current) => Math.min(sum,current), 0)
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0
+  } else {
+    return array.reduce((sum,current) => Math.max(sum,current), 0)
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0
+  } else {
+    return array.reduce((sum, current) => (sum + current), 0)  / array.length
+  }
 }
